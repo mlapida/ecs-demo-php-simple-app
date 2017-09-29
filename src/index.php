731d94becd7942d89eed3a7f667a35f063bea495
Mike Lapidakis <mlapida@amazon.com>
2017-09-28 21:50:35 -0500

    Reset for demo

--- src/index.php
+++ src/index.php
@@ -16,7 +16,7 @@
         <div class="container">
             <div class="hero-unit">
                 <h1>Simple PHP App</h1>
-                <h2>Congratulations Welcome to the meetup!</h2>
+                <h2>Congratulations!</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <p>The container is hosted on <?php echo $_SERVER['SERVER_ADDR']?></p>
